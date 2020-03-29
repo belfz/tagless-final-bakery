a22b943faefd580c4bc2c12f67493e52b3fabd3a
Marcin Baraniecki <marcin@swissborg.com>
2020-03-29 18:22:11 +0200

    minor rename

--- src/main/scala/bakery/MainIO.scala
+++ src/main/scala/bakery/MainIO.scala
@@ -6,25 +6,25 @@ import bakery.errors.ColdOvenError
 import bakery.interpreters.ElectricOven
 import bakery.models.Bread
 
-object TaglessFinalKitchen extends IOApp {
+object MainIO extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    val uncookedBread = Bread.prepare
+    val unbakedBread = Bread.prepare
     val coldOven = ElectricOven.make[IO]
 
     val bakingProgram = for {
       _ <- IO(println("Preheating the electric oven... done!"))
       preheatedOven <- coldOven.preheat
-      _ <- IO(println(s"Baking the $uncookedBread..."))
-      cookedBread <- preheatedOven.bake(uncookedBread)
+      _ <- IO(println(s"Baking the $unbakedBread..."))
+      bakedBread <- preheatedOven.bake(unbakedBread)
       _ <- IO(println(s"The bread grows twice in size!"))
-    } yield cookedBread
+    } yield bakedBread
 
     bakingProgram
       .recoverWith({
         case ColdOvenError =>
           IO(
             println("The oven was cold, your bread might not be good to eat :(")
-          ) >> IO(uncookedBread)
+          ) >> IO(unbakedBread)
       })
       .flatMap(bread => IO(println(s"Here's your bread: $bread")))
       .as(ExitCode.Success)
